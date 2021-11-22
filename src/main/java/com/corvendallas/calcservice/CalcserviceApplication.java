cefa7723a1334455cd7ea77b0ba50837484d63d8
CorvenDallas2020 <94530041+CorvenDallas2020@users.noreply.github.com>
2021-11-22 18:56:24 +0100

    Initial commit

---
+++ src/main/java/com/corvendallas/calcservice/CalcserviceApplication.java
@@ -0,0 +1,20 @@
+package com.corvendallas.calcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CalcserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcserviceApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+}
+}
